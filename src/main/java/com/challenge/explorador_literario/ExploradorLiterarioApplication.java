cd18dc777c6b420e3c8f16c7b38b4c2c6dece611
Alfonso Vidrio <154586838+AlfonsoVidrio@users.noreply.github.com>
2024-12-21 23:30:58 -0600

    feat: implement ApiClient to fetch data from external API using HttpClient

--- src/main/java/com/challenge/explorador_literario/ExploradorLiterarioApplication.java
+++ src/main/java/com/challenge/explorador_literario/ExploradorLiterarioApplication.java
@@ -1,5 +1,6 @@
 package com.challenge.explorador_literario;
 
+import com.challenge.explorador_literario.api.ApiClient;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ public class ExploradorLiterarioApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World!");
+		ApiClient apiClient = new ApiClient();
+		System.out.println(apiClient.getData("https://gutendex.com/books/"));
 	}
 }
